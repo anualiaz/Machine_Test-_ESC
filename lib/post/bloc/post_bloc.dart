4e3f3ca6b3153bb3fb55a77a7f125471fa6df19a
@anu2609 <anu@codeatech.com>
2024-01-18 15:05:30 +0530

    pages finished!!!

---
+++ lib/post/bloc/post_bloc.dart
@@ -0,0 +1,44 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:machine_test_esc/post/models/post.dart';
+import 'package:machine_test_esc/post/models/post_api.dart';
+
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc() : super(PostInitial());
+
+  @override
+  Stream<PostState> mapEventToState(PostEvent event) async* {
+    if (event is PostFetched) {
+      yield await _mapPostToState(state);
+    }
+    if (event is PostRefresh) {
+      yield PostInitial();
+
+      yield await _mapPostToState(state);
+    }
+  }
+
+  Future<PostState> _mapPostToState(PostState state) async {
+    List<Post> posts;
+
+    try {
+      if (state is PostInitial) {
+        posts = await PostApi.fetchPost(0, 10);
+        return PostLoaded(posts: posts);
+      }
+      PostLoaded postLoaded = state as PostLoaded;
+      posts = await PostApi.fetchPost(postLoaded.posts.length, 10);
+      return posts.isEmpty
+          ? postLoaded.copyWith(hasReachedMax: true)
+          : postLoaded.copyWith(posts: postLoaded.posts + posts);
+    } on Exception {
+      return PostError();
+    }
+  }
+}
